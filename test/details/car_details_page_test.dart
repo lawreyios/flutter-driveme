0121a0f5726842554a1626cbd0929f874cf18576
Lawrence Tan <lawtjh@gmail.com>
2020-04-03 23:14:16 +0800

    Update

--- test/details/car_details_page_test.dart
+++ test/details/car_details_page_test.dart
@@ -17,120 +17,36 @@ void main() {
 
   testWidgets('Unselected Car Details Page should be shown as Unselected', (WidgetTester tester) async {
     // TODO 35: Inject and Load Mock Car Data
-    carsListBloc.injectDataProviderForTest(MockCarDataProvider());
-    await carsListBloc.loadItems();
 
     // TODO 36: Load & Sort Mock Data for Verification
-    CarsList cars = await MockCarDataProvider().loadCars();
-    cars.items.sort(carsListBloc.alphabetiseItemsByTitleIgnoreCases);
 
     // TODO 37: Load and render Widget
-    await tester.pumpWidget(DetailsPageSelectedWrapper(2)); // Mercedes-Benz 2017
-    await tester.pump(Duration.zero);
 
     // TODO 38: Verify Car Details
-    final carDetailsKey = find.byKey(Key(CAR_DETAILS_KEY));
-    expect(carDetailsKey, findsOneWidget);
 
-    final pageTitleFinder = find.text(cars.items[1].title); // 2nd car in sorted list
-    expect(pageTitleFinder, findsOneWidget);
-
-    final notSelectedTextFinder = find.text(NOT_SELECTED_TITLE);
-    expect(notSelectedTextFinder, findsOneWidget);
-
-    final descriptionTextFinder = find.text(cars.items[1].description);
-    expect(descriptionTextFinder, findsOneWidget);
-
-    final featuresTitleTextFinder = find.text(FEATURES_TITLE);
-    expect(featuresTitleTextFinder, findsOneWidget);
-
-    var allFeatures = StringBuffer();
-    cars.items[1].features.forEach((feature) {
-      allFeatures.write('\n' + feature + '\n');
-    });
-
-    final featureTextFinder = find.text(allFeatures.toString());
-    await tester.ensureVisible(featureTextFinder);
-    expect(featureTextFinder, findsOneWidget);
-
-    final selectButtonFinder = find.text(SELECT_BUTTON);
-    await tester.ensureVisible(selectButtonFinder);
-    expect(selectButtonFinder, findsOneWidget);
   });
 
   testWidgets('Selected Car Details Page should be shown as Selected',
       (WidgetTester tester) async {
     // TODO 39: Inject and Load Mock Car Data
-    carsListBloc.injectDataProviderForTest(MockCarDataProvider());
-    await carsListBloc.loadItems();
 
     // TODO 40: Load and render Widget
-    await tester.pumpWidget(DetailsPageSelectedWrapper(3)); // Hyundai Sonata 2017
-    await tester.pump(Duration.zero);
 
     // TODO 41: Load Mock Data for Verification
-    CarsList actualCarsList = await MockCarDataProvider().loadCars();
-    List<Car> actualCars = actualCarsList.items;
 
     // TODO 42: First Car is Selected, so Verify that
-    final carDetailsKey = find.byKey(Key(CAR_DETAILS_KEY));
-    expect(carDetailsKey, findsOneWidget);
 
-    final pageTitleFinder = find.text(actualCars[2].title);
-    expect(pageTitleFinder, findsOneWidget);
-
-    final notSelectedTextFinder = find.text(SELECTED_TITLE);
-    expect(notSelectedTextFinder, findsOneWidget);
-
-    final descriptionTextFinder = find.text(actualCars[2].description);
-    expect(descriptionTextFinder, findsOneWidget);
-
-    final featuresTitleTextFinder = find.text(FEATURES_TITLE);
-    expect(featuresTitleTextFinder, findsOneWidget);
-
-    var actualFeaturesStringBuffer = StringBuffer();
-    actualCars[2].features.forEach((feature) {
-      actualFeaturesStringBuffer.write('\n' + feature + '\n');
-    });
-
-    final featuresTextFinder = find.text(actualFeaturesStringBuffer.toString());
-    await tester.ensureVisible(featuresTextFinder);
-    expect(featuresTextFinder, findsOneWidget);
-
-    final selectButtonFinder = find.text(REMOVE_BUTTON);
-    await tester.ensureVisible(selectButtonFinder);
-    expect(selectButtonFinder, findsOneWidget);
   });
 
   testWidgets('Selecting Car Updates the Widget', (WidgetTester tester) async {
     // TODO 43: Inject and Load Mock Car Data
-    carsListBloc.injectDataProviderForTest(MockCarDataProvider());
-    await carsListBloc.loadItems();
 
     // TODO 44: Load & Sort Mock Data for Verification
-    CarsList cars = await MockCarDataProvider().loadCars();
-    cars.items.sort(carsListBloc.alphabetiseItemsByTitleIgnoreCases);
 
     // TODO 45: Load and render Widget for the first car
-    await tester.pumpWidget(DetailsPageSelectedWrapper(2)); // Mercedes-Benz 2017
-    await tester.pump(Duration.zero);
 
     // TODO 46: Tap on Select and Deselect to ensure widget updates
-    final selectButtonFinder = find.text(SELECT_BUTTON);
-    await tester.ensureVisible(selectButtonFinder);
-    await tester.tap(selectButtonFinder);
 
-    await tester.pump(Duration.zero);
-
-    final deselectButtonFinder = find.text(REMOVE_BUTTON);
-    await tester.ensureVisible(deselectButtonFinder);
-    await tester.tap(deselectButtonFinder);
-
-    await tester.pump(Duration.zero);
-
-    final newSelectButtonFinder = find.text(SELECT_BUTTON);
-    await tester.ensureVisible(newSelectButtonFinder);
-    expect(newSelectButtonFinder, findsOneWidget);
   });
 }
 
--- test/list/cars_list_page_test.dart
+++ test/list/cars_list_page_test.dart
@@ -17,91 +17,50 @@ void main() {
       "Cars are displayed with summary details, and selected car is highlighted green.",
       (WidgetTester tester) async {
     // TODO 18: Inject and Load Mock Car Data
-    carsListBloc.injectDataProviderForTest(MockCarDataProvider());
 
     // TODO 19: Load & Sort Mock Data for Verification
-    CarsList cars = await MockCarDataProvider().loadCars();
-    cars.items.sort(carsListBloc.alphabetiseItemsByTitleIgnoreCases);
 
     // TODO 20: Load and render Widget
-    await tester.pumpWidget(ListPageWrapper());
-    await tester.pump(Duration.zero);
 
     // TODO 21: Check Cars List's component's existence via key
-    final carListKey = find.byKey(Key(CARS_LIST_KEY));
-    expect(carListKey, findsOneWidget);
 
     // TODO 23: Call Verify Car Details function
-    _verifyAllCarDetails(cars.items, tester);
 
     // TODO 24: Select a Car
-    carsListBloc.selectItem(1); // Selects Hyundai Sonata 2017
 
     // TODO 25: Verify that Car is highlighted in green
-    WidgetPredicate widgetSelectedPredicate = (Widget widget) =>
-        widget is Card && widget.color == Colors.blue.shade200;
-    WidgetPredicate widgetUnselectedPredicate =
-        (Widget widget) => widget is Card && widget.color == Colors.white;
 
-    expect(find.byWidgetPredicate(widgetSelectedPredicate), findsOneWidget);
-    expect(find.byWidgetPredicate(widgetUnselectedPredicate), findsNWidgets(5));
   });
 
   testWidgets('Proper error message is shown when an error occured',
       (WidgetTester tester) async {
     // TODO 26: Inject and Load Error Mock Car Data
-    carsListBloc.injectDataProviderForTest(MockCarDataProviderError());
 
     // TODO 27: Load and render Widget
-    await tester.pumpWidget(ListPageWrapper());
-    await tester.pump(Duration.zero);
 
     // TODO 28: Verify that Error Message is shown
-    final errorFinder =
-        find.text(ERROR_MESSAGE.replaceFirst(WILD_STRING, MOCK_ERROR_MESSAGE));
-    expect(errorFinder, findsOneWidget);
+
   });
 
   testWidgets(
       'After encountering an error, and stream is updated, Widget is also updated.',
       (WidgetTester tester) async {
     // TODO 29: Inject and Load Error Mock Car Data
-    carsListBloc.injectDataProviderForTest(MockCarDataProviderError());
 
     // TODO 30: Load and render Widget
-    await tester.pumpWidget(ListPageWrapper());
-    await tester.pump(Duration.zero);
 
     // TODO 31: Verify that Error Message is shown
-    final errorFinder =
-        find.text(ERROR_MESSAGE.replaceFirst(WILD_STRING, MOCK_ERROR_MESSAGE));
-    expect(errorFinder, findsOneWidget);
 
     // TODO 32: Inject and Load Mock Car Data
-    carsListBloc.injectDataProviderForTest(MockCarDataProvider());
-    await carsListBloc.loadItems();
 
     // TODO 33: Reload Widget
-    await tester.pump(Duration.zero);
 
     // TODO 34: Load and Verify Car Data
-    CarsList cars = await MockCarDataProvider().loadCars();
-    _verifyAllCarDetails(cars.items, tester);
+
   });
 }
 
 // TODO 22: Create a function to verify list's existence
-void _verifyAllCarDetails(List<Car> carsList, WidgetTester tester) async {
-  for (var car in carsList) {
-    final carTitleFinder = find.text(car.title);
-    final carPricePerDayFinder = find.text(PRICE_PER_DAY_TEXT.replaceFirst(
-        WILD_STRING, car.pricePerDay.toStringAsFixed(2)));
-    await tester.ensureVisible(carTitleFinder);
-    expect(carTitleFinder, findsOneWidget);
-    await tester.ensureVisible(carPricePerDayFinder);
-    expect(carPricePerDayFinder, findsOneWidget);
-  }
-}
 
 class ListPageWrapper extends StatelessWidget {
   @override
